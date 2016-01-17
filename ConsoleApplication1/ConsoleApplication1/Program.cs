a1bd7183836e529b5350953779739e85bb2228ae
孙乾程 <sqc1999@outlook.com>
2016-01-17 21:10:39 +0800

    u

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using static System.Console;
+
+namespace ConsoleApplication1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if (args.Length < 1 || !File.Exists(args[0])) return;
+			var md5 = MD5.Create();
+			var hash = md5.ComputeHash(new FileStream(args[0], FileMode.Open));
+			foreach (var i in hash) Write(i.ToString("x2"));
+			WriteLine();
+		}
+	}
+}
